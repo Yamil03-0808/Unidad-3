520061483aed30c4cd6021c11aa64f7a0bb03a09
Yamil03-0808 <sharonyamiletencizovazquez@gmail.com>
2025-05-29 22:19:23 -0600

    Add files via upload

---
+++ 20250520-081740.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    char cadena [10];
+    printf(“un int ocupa %d bytes”, sizeof(int));
+    printf(“un char ocupa %d bytes”, sizeof(char));
+    printf(“un float ocupa %d bytes”, sizeof(float));
+    printf(“un double ocupa %d bytes”, sizeof(double));
+    printf(“cadena ocupa %d bytes”, sizeof(cadena));
+    return 0;
+}
---
+++ 20250520-081830.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    char cadena[10];
+    printf("un int ocupa%d\n bytes",sizeof(int));
+    printf("un char ocupa%d\n bytes", sizeof(char));
+    printf("un float ocupa%d\n bytes",sizeof(float));
+    printf("un double ocupa%d\n bytes", sizeof(double));
+    printf("ordena ocupa%d\n bytes", sizeof(cadena));
+    return 0;
+}
